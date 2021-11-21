f92e212710c80bb7079e92075a13a231af3a2f8a
Yuliia <84233109+Yuliia-dev@users.noreply.github.com>
2021-11-21 12:16:22 +0200

    add task

---
+++ src/js/api-service.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.2.min.css';
+// const baseURL = 'https:pixabay.com/api/';
+const API_KEY = '24403288-52e492b65d436c39cf47d1c3c';
+// axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+export default class ApiService {
+  constructor() {
+    this.pageNum = 1;
+    this.searchValue = '';
+    this.URL = 'https://pixabay.com/api/';
+  }
+
+  fetchImg() {
+    // const baseURL = 'https:pixabay.com/api/';
+    return axios
+      .get(
+        `${this.URL}?key=${API_KEY}&q=${this.searchValue}&page=${this.pageNum}&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`,
+      )
+      .then(response => {
+        if (this.pageNum === 1) {
+          Notiflix.Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
+        }
+
+        return response.data;
+      });
+  }
+
+  pageAdd() {
+    this.pageNum += 1;
+  }
+
+  resetPage() {
+    this.pageNum = 1;
+  }
+
+  get query() {
+    return this.searchValue;
+  }
+
+  set query(newValue) {
+    this.searchValue = newValue;
+  }
+}
---
+++ src/js/index.js
@@ -0,0 +1,93 @@
+import ApiService from './api-service.js';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.2.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import imagesGal from '../../src/render-gal.hbs';
+import '../css/styles.css';
+
+const NewApiService = new ApiService();
+let step = 0;
+
+const formEl = document.querySelector('#search-form');
+const galleryEl = document.querySelector('.gallery');
+const loadMoreBth = document.querySelector('.load-more');
+
+galleryEl.addEventListener('click', onClickJustImage);
+formEl.addEventListener('submit', searchImg);
+loadMoreBth.addEventListener('click', onLoadMoreBtn);
+
+function searchImg(e) {
+  e.preventDefault();
+
+  NewApiService.query = e.currentTarget.elements.searchQuery.value.trim();
+  NewApiService.resetPage();
+  galleryEl.innerHTML = '';
+
+  if (NewApiService.query === '') {
+    step = 0;
+    loadMoreBth.classList.add('is-hidden');
+    galleryEl.innerHTML = '';
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.',
+    );
+    return;
+  }
+
+  NewApiService.fetchImg(NewApiService.query)
+    .then(showImg)
+    .catch(error => {
+      loadMoreBth.classList.add('is-hidden');
+      galleryEl.innerHTML = '';
+      Notiflix.Notify.failure(`Sorry, ${error.message}. Please try again.`);
+    });
+}
+
+function showImg(data) {
+  const markup = imagesGal(data);
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+
+  loadMoreBth.classList.remove('is-hidden');
+  const lightbox = new SimpleLightbox('.photo-card a');
+  scrollImg();
+
+  if (data.hits.length < 40) {
+    loadMoreBth.classList.add('is-hidden');
+    Notiflix.Notify.info('We are sorry, but you have reached the end of search results.');
+  }
+
+  if (data.hits.length < 1) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.',
+    );
+
+    return;
+  }
+}
+
+function scrollImg() {
+  const { height: cardHeight } = galleryEl.firstElementChild.getBoundingClientRect();
+  if (step !== 0)
+    window.scrollBy({
+      top: cardHeight * 1.5,
+      behavior: 'smooth',
+    });
+  step += 1;
+}
+
+function onClickJustImage(e) {
+  e.preventDefault();
+  const isImage = e.target.classList.contains('photo-card_img');
+  if (!isImage) {
+    return;
+  }
+}
+
+function onLoadMoreBtn(e) {
+  if (e) {
+    NewApiService.pageAdd();
+    NewApiService.fetchImg().then(showImg);
+    const lightbox = new SimpleLightbox('.photo-card a');
+    lightbox.refresh();
+  }
+}
